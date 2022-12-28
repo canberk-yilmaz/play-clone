a7f606258d5990cd25422849253147ac2359fdd8
canberk-yilmaz <cyilmaz089@gmail.com>
2022-12-29 00:12:15 +0300

    file move

---
+++ src/components/Home.vue
@@ -0,0 +1,18 @@
+<script setup>
+import AppStoreButton from "./AppStoreButton.vue";
+</script>
+
+<template>
+  <div
+    class="flex flex-col items-center justify-center px-[10%] lg:px-0 lg:max-w-[860px] mx-auto min-h-[50vh]"
+  >
+    <div
+      class="text-[color:#8cb9b4] text-4xl md:text-5xl lg:text-[56px] text-center"
+    >
+      The first native iOS design tool built for creating mobile products.
+    </div>
+    <AppStoreButton />
+  </div>
+</template>
+
+<style scoped></style>
