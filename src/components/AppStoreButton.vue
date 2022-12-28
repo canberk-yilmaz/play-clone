792977df8f058517fcb76fbbdf339af60998bfd6
canberk-yilmaz <cyilmaz089@gmail.com>
2022-12-29 00:12:51 +0300

    app store download component

---
+++ src/components/AppStoreButton.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="flex justify-center mt-10">
+    <div>
+      <a
+        href="https://apps.apple.com/us/app/id1479709805"
+        target="_blank"
+        class="link-block-8 w-inline-block"
+      ></a
+      ><img
+        src="https://global-uploads.webflow.com/5de7dc9925af690aa56c0736/60ee1293159d3a4c3e1e75ae_Download_on_the_App_Store_Badge_US-UK_RGB_blk_092917.svg"
+        loading="lazy"
+        alt=""
+      />
+    </div>
+  </div>
+</template>
